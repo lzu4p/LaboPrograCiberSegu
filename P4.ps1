b028efa005b289faaac71abc43fbc3c00371aec3
lzu4p <luis_6423@hotmail.com>
2020-12-02 15:53:51 -0600

    Practica 4

---
+++ P4.ps1
@@ -0,0 +1,12 @@
+﻿function theGame{
+    param([Parameter(Mandatory, ValueFromPipeline)] [string] $num)
+    if($num -eq "69"){
+        Write-Host ""
+        Write-Host "Viste eso?. Si, que habilidad."
+    } else {
+        Write-Host ""
+        Write-Host "Uy, casi le atinabas"
+    }
+}
+
+Read-Host "Number" | theGame
